cd2d43684feff091311f6234cc3f6b8f6e425cba
Emerson <emerson.vv75248@gmail.com>
2024-05-09 17:25:01 -0300

    wallet

--- wallet-spa/src/index.tsx
+++ wallet-spa/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import reportWebVitals from './reportWebVitals';
 import './index.css';
 import Layout from './components/layout';
 import { BrowserRouter } from 'react-router-dom';
@@ -19,7 +18,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
