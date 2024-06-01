e196fc07a919e4cf5d7e8854335a9063a4bc563b
Emerson Vieira <emersondont@gmail.com>
2024-06-01 00:35:04 -0300

    chore: Refactor fetchAPI error handling to parse error response as JSON

--- wallet-spa/src/services/fetchAPI.ts
+++ wallet-spa/src/services/fetchAPI.ts
@@ -2,16 +2,21 @@ export const fetchAPI = async (url: string, options?: RequestInit) => {
 	const response = await fetch(`${process.env.REACT_APP_API_URL}/${url}`, options).catch((error) => {
 		localStorage.removeItem('accessToken');
 	})
-		
-	if(response?.status === 401 && localStorage.getItem('accessToken')) {
+
+	if (response?.status === 401 && localStorage.getItem('accessToken')) {
 		localStorage.removeItem('accessToken');
 		window.location.href = '/login';
 	}
-	
+
 	if (!response?.ok) {
-		const errorBody = await response?.json(); // Ler o corpo da resposta como JSON
-		const errorMessage = errorBody.error; // Acessar a mensagem de erro
-		throw new Error(errorMessage);
+		const errorText = await response?.text(); // Ler o corpo da resposta como texto
+		let errorBody;
+		try {
+			errorBody = JSON.parse(errorText as string); // Tentar converter o texto em um objeto JSON
+		} catch {
+			throw errorText
+		}
+		throw new Error(errorBody.error);
 	}
 	return response;
 }
